f345f8330935ca3ffdd0f0fefd4dd522a2225ea0
pdxkcm <ken.mcgowan@gmail.com>
2018-01-30 18:28:30 -0800

    Initial commit, empty projects.

---
+++ Aughra.Content.Tests/ContentUrlTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aughra.Content.Tests
+{
+    public sealed class ContentUrlTests
+    {
+    }
+}
---
+++ Aughra.Content/ContentUrl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aughra.Content
+{
+    public sealed class ContentUrl
+    {
+        public ContentUrl(string url)
+        {
+        }
+
+        public static bool IsValidContentUrl(string url)
+        {
+            // This is busted, tests that prove that are forthcoming…
+            return !string.IsNullOrWhiteSpace(url);
+        }
+    }
+}
